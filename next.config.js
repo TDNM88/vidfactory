f6edc7e27c7e8bac5bc802ee020f92044fc81062
TDNM88 <dung.ngt1988@gmail.com>
2025-04-23 08:00:24 +0700

    rr

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   // Add allowedDevOrigins for ngrok
   allowedDevOrigins: [
     'https://*.ngrok-free.app',
