b776569312d57c6755aa5f8436e63d407deebcbe
TDNM88 <dung.ngt1988@gmail.com>
2025-04-27 17:08:09 +0700

    Uk

--- next.config.js
+++ next.config.js
@@ -8,7 +8,9 @@ const nextConfig = {
     'https://*.ngrok-free.app',
     'http://localhost:3000',
     'https://4b11-42-116-202-113.ngrok-free.app',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'http://192.168.1.4:3000',
+    'http://192.168.1.4'
   ],
 };
 
